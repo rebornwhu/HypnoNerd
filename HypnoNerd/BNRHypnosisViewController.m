0f9a32508531875374565e2f26e7d708a7e3b3b1
rebornwhu <rebornwhu@gmail.com>
2015-05-16 16:21:12 -0700

    Add tab bar items

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -11,6 +11,23 @@
 
 @implementation BNRHypnosisViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+    
+//    Should comment the following 2 lines otherwise it will gives
+//    a "parse error - expected expression"
+//    self = [super initWithNibName:nibNameOrNil
+//                           bundle:<#nibBundleOrNil#>];
+    if (self) {
+        self.tabBarItem.title = @"Hypnotize";
+        UIImage *i = [UIImage imageNamed:@"Hypno.pgn"];
+        self.tabBarItem.image = i;
+    }
+    
+    return self;
+}
+
 - (void)loadView
 {
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] init];
--- HypnoNerd/BNRReminderViewController.m
+++ HypnoNerd/BNRReminderViewController.m
@@ -16,6 +16,25 @@
 
 @implementation BNRReminderViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+    
+//    Should comment the following 2 lines otherwise it will gives
+//    a "parse error - expected expression"
+//    self = [super initWithNibName:<#nibNameOrNil#>
+//                           bundle:<#nibBundleOrNil#>];
+    if (self) {
+        UITabBarItem *tbi = self.tabBarItem;
+        tbi.title = @"Reminder";
+        
+        UIImage *i = [UIImage imageNamed:@"Time.png"];
+        tbi.image = i;
+    }
+    
+    return self;
+}
+
 - (IBAction)addReminder:(id)sender
 {
     NSDate *date = self.datePicker.date;
