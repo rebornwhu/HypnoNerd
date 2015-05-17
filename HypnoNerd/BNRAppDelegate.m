ebd8316b2362b932bff701811a971a34f09c016b
rebornwhu <rebornwhu@gmail.com>
2015-05-17 15:53:37 -0700

    Bronze challenge

--- HypnoNerd/BNRAppDelegate.m
+++ HypnoNerd/BNRAppDelegate.m
@@ -9,6 +9,7 @@
 #import "BNRAppDelegate.h"
 #import "BNRHypnosisViewController.h"
 #import "BNRReminderViewController.h"
+#import "BNRQuizViewController.h"
 
 @implementation BNRAppDelegate
 
@@ -21,8 +22,10 @@
     
     BNRReminderViewController *rvc = [[BNRReminderViewController alloc] init];
     
+    BNRQuizViewController *qvc = [[BNRQuizViewController alloc] init];
+    
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    tabBarController.viewControllers = @[hvc, rvc];
+    tabBarController.viewControllers = @[hvc, rvc, qvc];
     
     self.window.rootViewController = tabBarController;
     
---
+++ HypnoNerd/BNRQuizViewController.m
@@ -0,0 +1,79 @@
+//
+//  BNRViewController.m
+//  Quiz
+//
+//  Created by Xiao Lu on 5/5/15.
+//
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@property (nonatomic) NSInteger currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation BNRQuizViewController
+
+- (IBAction)showQuestion:(id)sender
+{
+    self.currentQuestionIndex++;
+    
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *queestion = self.questions[self.currentQuestionIndex];
+    
+    self.questionLabel.text = queestion;
+    
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        UITabBarItem *tbi = self.tabBarItem;
+        tbi.title = @"Quiz";
+//        UIImage *i = [UIImage imageNamed:@"Quiz.png"];
+//        tbi.image = i;
+        
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
