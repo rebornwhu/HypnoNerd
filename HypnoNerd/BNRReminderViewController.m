15812e4a56edb552512d8bd4010329cab736873c
rebornwhu <rebornwhu@gmail.com>
2015-05-17 15:33:48 -0700

    Enforce future time for reminder

--- HypnoNerd/BNRReminderViewController.m
+++ HypnoNerd/BNRReminderViewController.m
@@ -54,4 +54,10 @@
     NSLog(@"BNRReminderViewController loaded its view.");
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.datePicker.minimumDate = [NSDate dateWithTimeIntervalSinceNow:60];
+}
+
 @end
